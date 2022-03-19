57cdce96c3e910e4a39e89c2ef012a507f6711cd
Yannis Güdel <me@yannisguedel.ch>
2022-03-19 16:23:58 +0100

    asdf

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import xml from "fast-xml-parser";
+import { useEffect, useState } from "react";
+
+//const url = "http://127.0.0.1:8088/API/";
+const url = "/sample.xml";
+
+const inputWidth = 432;
+const inputHeight = 243;
+const titleHeight = 27;
+const gap = 10;
 
 function App() {
+  const parser = new xml.XMLParser();
+
+  const [inputs, setInputs] = useState([]);
+
+  useEffect(() => {
+    let isRunning = true;
+    (async () => {
+      while (isRunning) {
+        try {
+          const r = await fetch(url);
+          const body = await r.text();
+          const data = parser.parse(body);
+          console.log(data);
+          setInputs(data.vmix.inputs.input);
+        } catch (e) {
+          console.error(e);
+        }
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+      }
+    })();
+    return () => {
+      isRunning = false;
+    };
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{
+      display: "grid",
+      gridTemplateColumns: "repeat(4, " + inputWidth +  "px)",
+      columnGap: gap
+    }}>
+      {inputs.slice(0, 16).map((input, i) => {
+        return (
+          <div
+            key={i}
+            style={{ width: inputWidth, height: inputHeight + titleHeight }}
+          >
+            <div
+              style={{
+                height: titleHeight,
+                background: "#001D67",
+                color: "white",
+                fontWeight: "bold",
+                lineHeight: titleHeight + "px"
+              }}
+            >
+              {input["#text"] || input}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
